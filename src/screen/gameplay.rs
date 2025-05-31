cf1c220308c93c80f9ec5a1ff3a49315401b5d0a
Aaron Coleman <thecrafter1423@gmail.com>
2025-05-31 15:29:48 -0400

    Borderless and Player

--- src/screen/gameplay.rs
+++ src/screen/gameplay.rs
@@ -26,13 +26,33 @@ fn spawn_gameplay_screen(
         music_audio(&audio_settings, assets.music.clone()),
         DespawnOnExitState::<Screen>::Recursive,
     ));
+    commands.spawn((
+        Sprite {
+            image: assets.character_sprite.clone(),
+            texture_atlas: None,
+            color: Default::default(),
+            flip_x: false,
+            flip_y: false,
+            custom_size: None,
+            rect: None,
+            anchor: Default::default(),
+            image_mode: Default::default(),
+        },
+        Player,
+        DespawnOnExitState::<Screen>::Recursive,
+    ));
 }
+
+#[derive(Component)]
+struct Player;
 
 #[derive(AssetCollection, Resource, Reflect, Default)]
 #[reflect(Resource)]
 pub struct GameplayAssets {
     #[asset(path = "audio/music/summer.ogg")]
     music: Handle<AudioSource>,
+    #[asset(path = "image/Player.png")]
+    character_sprite: Handle<Image>,
 }
 
 impl Configure for GameplayAssets {
@@ -46,7 +66,14 @@ impl Configure for GameplayAssets {
 pub enum GameplayAction {
     Pause,
     CloseMenu,
+    MoveLeft,
+    MoveRight,
+    MoveUp,
+    MoveDown,
+    Sprint,
 }
+
+const WALKING_SPEED: f32 = 1.0;
 
 impl Configure for GameplayAction {
     fn configure(app: &mut App) {
@@ -56,7 +83,12 @@ impl Configure for GameplayAction {
                 .with(Self::Pause, GamepadButton::Start)
                 .with(Self::Pause, KeyCode::Escape)
                 .with(Self::Pause, KeyCode::KeyP)
-                .with(Self::CloseMenu, KeyCode::KeyP),
+                .with(Self::CloseMenu, KeyCode::KeyP)
+                .with(Self::MoveUp, KeyCode::KeyW)
+                .with(Self::MoveLeft, KeyCode::KeyA)
+                .with(Self::MoveDown, KeyCode::KeyS)
+                .with(Self::MoveRight, KeyCode::KeyD)
+                .with(Self::Sprint, KeyCode::ShiftLeft),
         );
         app.add_plugins(InputManagerPlugin::<Self>::default());
         app.add_systems(
@@ -68,9 +100,44 @@ impl Configure for GameplayAction {
                 Menu::clear
                     .in_set(UpdateSystems::RecordInput)
                     .run_if(Menu::is_enabled.and(action_just_pressed(Self::CloseMenu))),
+                move_player_right
+                    .in_set(UpdateSystems::RecordInput)
+                    .run_if(action_pressed(Self::MoveRight)),
+                move_player_left
+                    .in_set(UpdateSystems::RecordInput)
+                    .run_if(action_pressed(Self::MoveLeft)),
+                move_player_up
+                    .in_set(UpdateSystems::RecordInput)
+                    .run_if(action_pressed(Self::MoveUp)),
+                move_player_down
+                    .in_set(UpdateSystems::RecordInput)
+                    .run_if(action_pressed(Self::MoveDown)),
+                
             )),
         );
     }
+}
+
+fn move_player_right(query: Query<&mut Transform, With<Player>>) {
+    move_player(query, vec3(WALKING_SPEED, 0.0, 0.0));
+}
+
+fn move_player_left(query: Query<&mut Transform, With<Player>>) {
+    move_player(query, vec3(-WALKING_SPEED, 0.0, 0.0));
+}
+
+fn move_player_up(query: Query<&mut Transform, With<Player>>) {
+    move_player(query, vec3(0.0, WALKING_SPEED, 0.0));
+}
+
+fn move_player_down(query: Query<&mut Transform, With<Player>>) {
+    move_player(query, vec3(0.0, -WALKING_SPEED, 0.0));
+}
+
+fn move_player(mut query: Query<&mut Transform, With<Player>>, direction: Vec3) {
+    query.iter_mut().for_each(|mut transform| {
+        transform.translation += direction;
+    })
 }
 
 fn spawn_pause_overlay(mut commands: Commands) {
