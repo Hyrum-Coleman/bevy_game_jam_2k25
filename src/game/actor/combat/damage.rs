6d5200dbf8ed733e21cce42ee21ef7579c4193aa
Hyrum <hyrum.coleman@gmail.com>
2025-06-06 19:57:37 -0400

    add on damage event and function that receives event

--- src/game/actor/combat/damage.rs
+++ src/game/actor/combat/damage.rs
@@ -1,3 +1,4 @@
+use crate::game::actor::combat::health::Health;
 use crate::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
@@ -14,3 +15,27 @@ impl Configure for Damage {
     }
 }
 
+#[derive(Event, Reflect, Debug)]
+pub struct OnDamage(pub f32);
+
+impl Configure for OnDamage {
+    fn configure(app: &mut App) {
+        app.register_type::<Self>();
+        app.add_observer(on_damage);
+    }
+}
+
+fn on_damage(trigger: Trigger<OnDamage>, mut health_query: Query<&mut Health>) {
+    let target = r!(trigger.get_target());
+    let target_health = r!(health_query.get_mut(target));
+    target_health.current -= trigger.0;
+}
+
+fn deal_damage_on_collision(
+    mut commands: Commands,
+    trigger: Trigger<OnCollisionStart>,
+    damage_query: Query<&Damage>,
+    health_query: Query<(), With<Health>>,
+) {
+    
+}
