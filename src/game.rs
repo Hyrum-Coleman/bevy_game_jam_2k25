00413138ad2b8a281d889c37ad67668fe16e5271
Hyrum <hyrum.coleman@gmail.com>
2025-06-07 16:00:31 -0400

    cargo fmt

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,8 @@
 use crate::prelude::*;
 
 pub mod actor;
-pub mod world;
 mod effects;
+pub mod world;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins((actor::plugin, world::plugin, effects::plugin));
