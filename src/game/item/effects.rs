027c7e106600a9aaa43b2f2909948e2c38702fb5
Hyrum <hyrum.coleman@gmail.com>
2025-06-08 16:45:59 -0400

    add damage over time base

--- src/game/item/effects.rs
+++ src/game/item/effects.rs
@@ -1,8 +1,9 @@
 pub mod fire;
 pub mod life_steal;
+pub mod damage_over_time;
 
 use crate::prelude::*;
 
 pub(in crate::game) fn plugin(app: &mut App) {
-    app.add_plugins((life_steal::plugin, fire::plugin));
+    app.add_plugins((life_steal::plugin, fire::plugin, damage_over_time::plugin));
 }
